0f2897ad02ca9947451a3054d752b85909ee5dff
FelipeGrellmannSouza <felipegrellmann1@gmail.com>
2024-09-09 08:01:22 -0300

    render setup - add tsc

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import { Pet } from '../models/pet';
 import {createMenuObject} from '../helpers/createMenuObject';
 
-
 export const search = (req: Request, res: Response) => {
     let query: string = req.query.q as string;
 
